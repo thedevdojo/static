dc11402613cb7d942f542e69fec468b9c8f49b89
Tony Lea <tnylea@gmail.com>
2023-08-31 18:54:18 -0400

    making sure we can have multiple live reload working

--- src/dev.js
+++ src/dev.js
@@ -10,6 +10,7 @@ const net = require('net');
 const currentDirectory = process.cwd();
 const parser = require('./parser.js');
 const assets = require('./assets.js');
+const liveReloadDefaultPort = 35729;
 
 const esbuild = require('esbuild');
 
@@ -20,37 +21,49 @@ module.exports = {
 
         this.getAvailablePort(port).then((availablePort) => {
 
-            const liveReloadServer = livereload.createServer();
-            liveReloadServer.watch(currentDirectory + "/src");
-            liveReloadServer.watch(currentDirectory + "/_site/assets");
-            liveReloadServer.server.once("connection", () => {
-                setTimeout(() => {
-                    liveReloadServer.refresh("/");
-                }, 100);
-            });
-
-            app.use(connectLiveReload());
-
-            app.use('/assets', express.static(path.join(currentDirectory, '_site/assets')))
+            // get available port for the Live Reload Server
+            this.getAvailablePort(liveReloadDefaultPort).then((liveReloadAvailablePort) => {
+                
             
-            app.get('/*', (req, res) => {
-                const route = req.path === '/' ? '/index' : req.path;
-                let pagePath = path.join(currentDirectory, './pages', route + '.html');
-                if (!fs.existsSync(pagePath)) {
-                    pagePath = path.join(currentDirectory, './pages', route, 'index.html');
-                    if (!fs.existsSync(pagePath)) {
-                        res.status(404).send('Page not found');
-                        return;
-                    }
-                }
-    
-                const content = parser.processFile(pagePath);
-                res.send(content);
-            });
+                const liveReloadServer = livereload.createServer({
+                    port: liveReloadAvailablePort
+                });
+                liveReloadServer.watch(currentDirectory + "/src");
+                liveReloadServer.watch(currentDirectory + "/_site/assets");
+                liveReloadServer.server.once("connection", () => {
+                    setTimeout(() => {
+                        liveReloadServer.refresh("/");
+                    }, 100);
+                });
 
-            app.listen(availablePort, () => {
-                console.log(`Server running at http://localhost:${availablePort}`);
+                app.use(connectLiveReload());
+
+                app.use('/assets', express.static(path.join(currentDirectory, '_site/assets')))
+                
+                app.get('/*', (req, res) => {
+                    const route = req.path === '/' ? '/index' : req.path;
+                    let pagePath = path.join(currentDirectory, './pages', route + '.html');
+                    if (!fs.existsSync(pagePath)) {
+                        pagePath = path.join(currentDirectory, './pages', route, 'index.html');
+                        if (!fs.existsSync(pagePath)) {
+                            res.status(404).send('Page not found');
+                            return;
+                        }
+                    }
+        
+                    const content = parser.processFile(pagePath);
+                    res.send(content);
+                });
+
+                app.listen(availablePort, () => {
+                    console.log(`Server running at http://localhost:${availablePort}`);
+                });
+
+            }).catch((error) => {
+                console.log('error finding available port for liveReload');
+                console.log(error);
             });
+            
         }).catch((error) => {
             console.log('error finding available port number');
             console.log(error);
@@ -58,13 +71,14 @@ module.exports = {
         
     },
     getAvailablePort(port) {
+        
         return new Promise((resolve, reject) => {
             const server = net.createServer();
     
             server.once('error', (err) => {
                 if (err.code === 'EADDRINUSE') {
                     // port is currently in use, try the next one
-                    resolve(getAvailablePort(port + 1));
+                    resolve(this.getAvailablePort(port + 1));
                 } else {
                     // some other error, reject the promise
                     reject(err);
